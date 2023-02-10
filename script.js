7c3646fbef54de0219fd427756200ae5601695fa
Manish <manish@dowellresearch.in>
2023-02-11 04:15:37 +0530

    Added the regional time

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
+function displayTime() {
+  var date = new Date();
+  var time = date.toTimeString().split(' ')[0];
+  var milliseconds = date.getMilliseconds();
+
+  var formattedTime = time + ":" + milliseconds;
+  return formattedTime;
+}
+
 // https://100009.pythonanywhere.com/dowellclock/
 // http://127.0.0.1:8000/dowellclock/
-
 
 fetch('https://100009.pythonanywhere.com/dowellclock/', {
   method: 'POST',
@@ -21,15 +29,20 @@ fetch('https://100009.pythonanywhere.com/dowellclock/', {
   })
   .then(data => {
     document.getElementById("dowellTime").innerHTML = "Dowell Time: " + data.dowelltime;
-    let intervalId = 1;
-    setInterval(() => {
-        data.dowelltime = data.dowelltime + 1 / 1000;
-        intervalId = data.dowelltime
-        document.getElementById("updatetime").innerHTML = "Update Time: " + intervalId;
-      }, 1000);
     
     document.getElementById("unixtime").innerHTML = "Regional Time: " + data.currenttime;
     
+    let intervalId = 1;
+    setInterval(() => {
+        data.dowelltime = (data.dowelltime) + (new Date().getTime());
+        intervalId = data.dowelltime
+        document.getElementById("updatetime").innerHTML = "Update DowellTime: " + Math.floor(intervalId);
+      }, 1000);
+      
+    setInterval(() => {
+      document.getElementById("Updateregional").innerHTML = "Updated regional Time: " + displayTime()}, 1000);
+      
+          
   })
   .catch(error => {
     console.error('There was a problem with the fetch operation:', error);
