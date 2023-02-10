194a09eb181e5f716cc3fd9abfc154e8a1ab54ba
Manish <manish@dowellresearch.in>
2023-02-10 18:51:18 +0530

    v2

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-const n = 1;
+
 // https://100009.pythonanywhere.com/dowellclock/
 // http://127.0.0.1:8000/dowellclock/
-const intervalId = setInterval(() => {
+
+
 fetch('https://100009.pythonanywhere.com/dowellclock/', {
-    method: 'POST',
-    headers: {
+  method: 'POST',
+  headers: {
     'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
+  },
+  body: JSON.stringify({
     "timezone": timezone
-    })
+  })
 })
-    .then(response => {
+  .then(response => {
     if (!response.ok) {
-        throw new Error('Network response was not ok');
+      throw new Error('Network response was not ok');
     }
     return response.json();
-    })
-    .then(data => {
-        document.getElementById("dowellTime").innerHTML = "Dowell Time: " + data.dowelltime;
-        document.getElementById("unixtime").innerHTML = "Regional Time: " + data.currenttime;
-    })
-    .catch(error => {
+  })
+  .then(data => {
+    document.getElementById("dowellTime").innerHTML = "Dowell Time: " + data.dowelltime;
+    let intervalId = 1;
+    setInterval(() => {
+        data.dowelltime = data.dowelltime + 1 / 1000;
+        intervalId = data.dowelltime
+        document.getElementById("updatetime").innerHTML = "Update Time: " + intervalId;
+      }, 1000);
+    
+    document.getElementById("unixtime").innerHTML = "Regional Time: " + data.currenttime;
+    
+  })
+  .catch(error => {
     console.error('There was a problem with the fetch operation:', error);
-    });
-}, n * 1000);
+  });
