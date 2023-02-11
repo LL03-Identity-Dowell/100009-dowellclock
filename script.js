031bc3254132d23289c53daecd05cb25feb35cac
Manish <manish@dowellresearch.in>
2023-02-11 15:10:08 +0530

    Pushed all the requirements

--- script.js
+++ script.js
@@ -1,49 +1,52 @@
+
 // START CLOCK SCRIPT
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-var apires;
+let apires, startTime;
+
 fetch('https://100009.pythonanywhere.com/dowellclock/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      "timezone": timezone
-    })
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    timezone
   })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {   document.getElementById("dowellTime").innerHTML = "Dowell Time: " + data.dowelltime;
-     apires = data.dowelltime;
-    })
-    .catch(error => {
-      console.error('There was a problem with the fetch operation:', error);
-    });
+})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    document.getElementById("dowellTime").innerHTML = "Dowell Time: " + data.dowelltime;
+    apires = data.dowelltime;
+    startTime = Date.now();
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
 
 Number.prototype.pad = function(n) {
   for (var r = this.toString(); r.length < n; r = 0 + r);
   return r;
 };
 
-function updateClock() {
+const updateClock = () => {
   apires++;
-  var now = new Date();
-  var milli = now.getMilliseconds(),
+  const now = new Date();
+  const milli = now.getMilliseconds(),
     sec = now.getSeconds(),
     min = now.getMinutes(),
     hou = now.getHours();
-  var tags = ["dowellup", "h", "m", "s", "mi"],
+  const tags = ["dowellup", "h", "m", "s", "mi"],
     corr = [apires, hou.pad(2), min.pad(2), sec.pad(2), milli];
-  for (var i = 0; i < tags.length; i++)
+  for (let i = 0; i < tags.length; i++)
     document.getElementById(tags[i]).firstChild.nodeValue = corr[i];
-}
+  document.getElementById("sessionTime").innerHTML = (Date.now() - startTime) / 1000;
+};
 
-function initClock() {
+const initClock = () => {
   updateClock();
-  window.setInterval("updateClock()", 1);
-}
-
-// END CLOCK SCRIPT
+  setInterval(updateClock, 1);
+};
