eace951b184865350ebc87f362103c72f4e70703
Manish <manish@dowellresearch.in>
2023-02-11 10:36:08 +0530

    Corrected By nitesh

--- script.js
+++ script.js
@@ -1,49 +1,51 @@
+// START CLOCK SCRIPT
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+var apires;
+fetch('https://100009.pythonanywhere.com/dowellclock/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "timezone": timezone
+    })
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {   document.getElementById("dowellTime").innerHTML = "Dowell Time: " + data.dowelltime;
+     apires = data.dowelltime;
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
 
-function displayTime() {
-  var date = new Date();
-  var time = date.toTimeString().split(' ')[0];
-  var milliseconds = date.getMilliseconds();
+Number.prototype.pad = function(n) {
+  for (var r = this.toString(); r.length < n; r = 0 + r);
+  return r;
+};
 
-  var formattedTime = time + ":" + milliseconds;
-  return formattedTime;
+function updateClock() {
+  apires++;
+  var now = new Date();
+  var unix = new Date().getTime();
+  var dowelltime = unix - 1609459200;
+  var milli = now.getMilliseconds(),
+    sec = now.getSeconds(),
+    min = now.getMinutes(),
+    hou = now.getHours();
+  var tags = ["unix", "dowell", "dowellup", "h", "m", "s", "mi"],
+    corr = [unix, dowelltime, apires, hou.pad(2), min.pad(2), sec.pad(2), milli];
+  for (var i = 0; i < tags.length; i++)
+    document.getElementById(tags[i]).firstChild.nodeValue = corr[i];
 }
 
-// https://100009.pythonanywhere.com/dowellclock/
-// http://127.0.0.1:8000/dowellclock/
+function initClock() {
+  updateClock();
+  window.setInterval("updateClock()", 1);
+}
 
-fetch('https://100009.pythonanywhere.com/dowellclock/', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    "timezone": timezone
-  })
-})
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    document.getElementById("dowellTime").innerHTML = "Dowell Time: " + data.dowelltime;
-    
-    document.getElementById("unixtime").innerHTML = "Regional Time: " + data.currenttime;
-    
-    let intervalId = 1;
-    setInterval(() => {
-        data.dowelltime = (data.dowelltime) + (new Date().getTime());
-        intervalId = data.dowelltime
-        document.getElementById("updatetime").innerHTML = "Update DowellTime: " + Math.floor(intervalId);
-      }, 1000);
-      
-    setInterval(() => {
-      document.getElementById("Updateregional").innerHTML = "Updated regional Time: " + displayTime()}, 1000);
-      
-          
-  })
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-  });
+// END CLOCK SCRIPT
