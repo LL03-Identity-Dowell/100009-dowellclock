bbcc2075d0bcdd1464782f986aece308044207e8
Manish <manish@dowellresearch.in>
2023-02-15 13:42:23 +0530

    Done

--- scripts.js
+++ scripts.js
@@ -1,99 +1,99 @@
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-const dowelltime = document.getElementById('dowelltime');
-const updatingtime = document.getElementById('updatedowelltime');
-const updatemillisecond = document.getElementById('updatemillisecond');
-const starttime = document.getElementById('start_time');
-const sessiontime = document.getElementById('sessiontime');
-const oneMinutetime = document.getElementById('oneMinutetime');
-const regional = document.getElementById('regional');
-const systemhour = document.getElementById('system-h');
-const systemminute = document.getElementById('system-m');
-const systemsecond = document.getElementById('system-s');
-const systemmillisecond = document.getElementById('system-mi');
+const dowelltime = document.getElementById("dowelltime");
+const updatingtime = document.getElementById("updatedowelltime");
+const updatemillisecond = document.getElementById("updatemillisecond");
+const starttime = document.getElementById("start_time");
+const sessiontime = document.getElementById("sessiontime");
+const hour = document.getElementById("h");
+const minute = document.getElementById("m");
+const second = document.getElementById("s");
+const millisecond = document.getElementById("mi");
+const regionaltime = document.getElementById("regionaltime");
+
+
+let update = 0,
+      up = 0;
 
 const fetchData = async () => {
-    try {
-      const response = await fetch('https://100009.pythonanywhere.com/dowellclock/', {
-        method: 'POST',
+  try {
+    const response = await fetch(
+      "https://100009.pythonanywhere.com/dowellclock/",
+      {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json"
         },
         body: JSON.stringify({
           timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
         })
-      });
-  
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
       }
-  
-      const data = await response.json();
-      console.log(data);
-      return data;
-    } catch (error) {
-      console.error('There was a problem with the fetch operation:', error);
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
-  };
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+};
 // dowell time
 fetchData().then((data) => {
-    // set the initial dowelltime and start time values
-    dowelltime.innerHTML = data.dowelltime;
-    starttime.innerHTML = data.currenttime;
+  // set the initial dowelltime and start time values
+  dowelltime.innerHTML = data.dowelltime;
+  starttime.innerHTML = data.currenttime;
+  updatetime();
 
-    // update oneMinutetime after 1 minute
-    oneMinutetime.innerHTML= data.dowelltime;
-    setInterval(() => {
-        fetchData().then((data) => {
-            oneMinutetime.innerHTML = data.dowelltime;
-        });
-    }, 60000);
+  // update oneMinutetime after 1 minute
+  updatingtime.innerHTML = data.dowelltime;
+  setInterval(() => {
+    fetchData().then((data) => {
+      updatingtime.innerHTML = data.dowelltime;
+      update = 0, up = 0;
+    });
+  }, 60000);
 
-    // update the updating time values
-    updatingtime.innerHTML = data.dowelltime;
-    setInterval(() => {
-        fetchData().then((data) => {
-            updatingtime.innerHTML = data.dowelltime;
-        });
-    }, 60000);
+  // regional time
+  let datetimeString = data.current_time;
+  regionaltime.innerHTML = datetimeString.substr(11, 12);
 
-    // correct this nitesh
-    let updatemilliseconds = 0;
+  setInterval(() => {
+    fetchData().then((data) => {
+      const datetimeString = data.current_time;
+      const timeString = datetimeString.substr(11, 12);
+      regionaltime.innerHTML = timeString;
+    });
+  }, 60000);
+
+  // system time
+  setInterval(() => {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var milliseconds = currentTime.getMilliseconds();
+
+    hour.innerHTML = hours;
+    minute.innerHTML = minutes;
+    second.innerHTML = seconds;
+    millisecond.innerHTML = milliseconds;
+  }, 1);
+
+  function updatetime() {
     const updateInterval = setInterval(() => {
-      updatemilliseconds = 0; // reset the value to zero
-    }, 60000);
+      update++;
+      setInterval(() => {
+        var curr = new Date();
+        let mill = curr.getMilliseconds();
 
-    setInterval(() => {
-      updatemilliseconds += 1;
-      updatemillisecond.innerHTML = updatemilliseconds;
-    }, 1); 
-      
-    // regional time
-    let datetimeString = data.current_time;
-    regional.innerHTML = datetimeString.substr(11, 12);
-
-    setInterval(() => {
-      fetchData().then((data) => {
-        const datetimeString = data.current_time;
-        const timeString = datetimeString.substr(11, 12);
-        regional.innerHTML = timeString;
-      });
-    }, 60000);
-
-    // system time
-    setInterval(()=> {
-      var currentTime = new Date();
-      var hours = currentTime.getHours();
-      var minutes = currentTime.getMinutes();
-      var seconds = currentTime.getSeconds();
-      var milliseconds = currentTime.getMilliseconds();
-
-      systemhour.innerHTML = hours;
-      systemminute.innerHTML = minutes;
-      systemsecond.innerHTML = seconds;
-      systemmillisecond.innerHTML = milliseconds;
-  },1);
-
+        up = update + "" + mill;
+        updatemillisecond.innerHTML = up;
+      }, 1);
+    }, 1000);
+  }
   // session time 
   setInterval(()=> {
     var currentTime = new Date();
@@ -116,5 +116,4 @@ fetchData().then((data) => {
     sessiontime.innerHTML = formattedTime;
 
   },1);
-
 });
